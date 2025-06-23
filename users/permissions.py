a7eab44f4c9bfbda2d7cdf11574dde7dfdb958b8
github-main-user <kolya.skripn@mail.ru>
2025-06-23 23:02:06 +0300

    chore: create custom IsOwner permission

---
+++ users/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    """Validates if user in request is owner of the object."""
+
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
