df4eac62ad40c9c8d9bf8369ac56733a20d6b199
github-main-user <kolya.skripn@mail.ru>
2025-06-24 00:09:13 +0300

    chore: include the new serializer to habit views

--- habits/views.py
+++ habits/views.py
@@ -3,14 +3,18 @@ from rest_framework import generics
 from users.permissions import IsOwner
 
 from .models import Habit
+from .serializers import HabitSerializer
 
 
 class HabitListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = HabitSerializer
 
     def get_queryset(self):
         return Habit.objects.filter(user=self.request.user)
 
 
 class HabitRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = HabitSerializer
+
     queryset = Habit.objects.all()
     permission_classes = [IsOwner]
