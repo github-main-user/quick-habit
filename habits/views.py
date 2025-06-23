25fbdd2943b8d91f257895fcef481176d4ee4fc2
github-main-user <kolya.skripn@mail.ru>
2025-06-24 00:30:18 +0300

    chore: connect paginator to habit views

--- habits/views.py
+++ habits/views.py
@@ -3,11 +3,13 @@ from rest_framework import generics
 from users.permissions import IsOwner
 
 from .models import Habit
+from .paginators import HabitPaginator
 from .serializers import HabitSerializer
 
 
 class HabitListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = HabitSerializer
+    pagination_class = HabitPaginator
 
     def get_queryset(self):
         return Habit.objects.filter(user=self.request.user)
