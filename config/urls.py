013f465a87a984cc69f200936e3f8af0da89e100
github-main-user <kolya.skripn@mail.ru>
2025-06-24 00:44:11 +0300

    feat: add and setup simple jwt auth

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/habits/", include("habits.urls", namespace="habits")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
