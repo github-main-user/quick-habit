327ab058c2fa52d71a426cb3a616d33833392340
github-main-user <kolya.skripn@mail.ru>
2025-06-24 01:13:26 +0300

    refactor: move jwt urls to users app

--- config/urls.py
+++ config/urls.py
@@ -5,14 +5,10 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     # admin
     path("admin/", admin.site.urls),
-    # JWT
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # docs
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(), name="swagger-ui"),
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .apps import UsersConfig
 from .views import RegisterView
@@ -8,4 +9,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
