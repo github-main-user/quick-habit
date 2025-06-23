35b8857d615e1f3f53d2834186bf9f136d5f13ba
github-main-user <kolya.skripn@mail.ru>
2025-06-24 00:17:03 +0300

    chore: connect Habit model to admin panel

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = [
+        "owner",
+        "place",
+        "time",
+        "action",
+        "is_pleasant",
+        "execution_time",
+        "is_public",
+    ]
+    list_filter = ["owner", "is_pleasant", "is_public"]
+    search_fields = ["place", "time", "action"]
